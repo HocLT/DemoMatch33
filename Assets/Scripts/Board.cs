5ffa547ca6eacf3ec273f396ee47b2047176184b
HocLT <hdquang6@gmail.com>
2023-10-11 13:23:21 +0700

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public GameObject bgTilePrefab;
+
+    public Gem[] gems;
+
+    public Gem[,] allGems;  // lưu lại thông tin các gem đang có trên board
+    // Start is called before the first frame update
+    void Start()
+    {
+        allGems = new Gem[width, height];
+
+        Setup();
+    }
+
+    // Update is called once per frame
+    void Setup()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                GameObject bgTile = Instantiate(bgTilePrefab, new Vector3(x, y, 0f), Quaternion.identity);  // Quaternion trong unity biểu diễn góc xoay
+                bgTile.transform.parent = transform;
+                bgTile.name = $"Background - {x}, {y}";
+
+                // sinh ngẫu nhiên 1 số từ 0 - 4
+                int gemToUse = UnityEngine.Random.Range(0, gems.Length);
+                // gem[gemToUse] => thể hiện Gem Object đang sử dụng
+                SpawnGem(new Vector2Int(x, y), gems[gemToUse]);
+            }
+        }
+    }
+
+    void SpawnGem(Vector2Int pos, Gem gemToSpawn)
+    {
+        Gem gem = Instantiate(gemToSpawn, new Vector3(pos.x, pos.y, 0f), Quaternion.identity);
+        gem.transform.parent = transform;
+        gem.name = $"Gem - {pos.x}, {pos.y}";
+
+        allGems[pos.x, pos.y] = gem;
+
+        // lưu thông tin cho gem
+        gem.Setup(pos, this);
+    }
+}
---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    //[HideInInspector]
+    public Vector2Int pos;
+
+    //[HideInInspector]
+    public Board board;
+
+    Vector3 firstTouchPosition, finalTouchPosition;
+    bool mousePressed = false;
+    float swipeAngle = 0f;
+
+    Gem otherGem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // swap gem
+
+        // xử lý sự kiện mouse release
+        if (mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
+    }
+
+    public void Setup(Vector2Int pos, Board board)
+    {
+        this.pos = pos;
+        this.board = board;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;    // đang nhấn đè vào nút chuột trái
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(
+            finalTouchPosition.y - firstTouchPosition.y,
+            finalTouchPosition.x - firstTouchPosition.x
+            );
+        swipeAngle = swipeAngle * 180 / Mathf.PI;
+        //Debug.Log(swipeAngle);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && pos.x < board.width - 1)
+        {
+            otherGem = board.allGems[pos.x + 1, pos.y];
+            otherGem.pos.x--;
+            pos.x++;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && pos.y < board.height - 1)
+        {
+            otherGem = board.allGems[pos.x, pos.y + 1];
+            otherGem.pos.y--;
+            pos.y++;
+        }
+        else if (swipeAngle > -135 && swipeAngle <= -45 && pos.y > 0)
+        {
+            otherGem = board.allGems[pos.x, pos.y - 1];
+            otherGem.pos.y++;
+            pos.y--;
+        }
+        else if ((swipeAngle <= -135 || swipeAngle > 135) && pos.x > 0)
+        {
+            otherGem = board.allGems[pos.x - 1, pos.y];
+            otherGem.pos.x++;
+            pos.x--;
+        }
+
+        // update lại thông tin của các gem
+        board.allGems[pos.x, pos.y] = this;
+        board.allGems[otherGem.pos.x, otherGem.pos.y] = otherGem;
+    }
+}
